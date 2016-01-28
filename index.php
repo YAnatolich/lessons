5b631cfe275f07facb7f35c0e2af864cd6422414
YAnatolich <ja.kalugin@gmail.com>
2016-01-28 10:09:56 +0200

    базовые шаблоны html

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include("application/view/tmp_head.php");
+include("application/view/tmp_lft_colon.php");
+include("application/view/tmp_content.php");
+include("application/view/tmp_bottom.php");
