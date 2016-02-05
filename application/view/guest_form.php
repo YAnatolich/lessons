be5ebf1f72f1c8ab83f041499dfe86614919f1c5
YAnatolich <ja.kalugin@gmail.com>
2016-02-05 22:11:48 +0200

    базовые шаблоны html

--- application/view/guest_form.php
+++ application/view/guest_form.php
@@ -3,5 +3,5 @@
   <p><input type="radio" name="answer" value="a1">Офицерский состав<Br>
   <input type="radio" name="answer" value="a2">Операционная система<Br>
   <input type="radio" name="answer" value="a3">Большой полосатый мух</p>
-  <p><input type="submit"></p>
+  <p><input type="submit"></p>sdfsdf
  </form> 
