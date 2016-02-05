604cd6f9762cda7bc006f7369c34b51e73076871
YAnatolich <ja.kalugin@gmail.com>
2016-02-05 23:39:17 +0200

    add model

--- application/view/guest_form.php
+++ application/view/guest_form.php
@@ -1,4 +1,9 @@
 <form action="GuestController.php" method="POST">
+
+    <input name="name">Введите имя 
+    <input name="email">Введите мыло
+    <textarea>
+    </textarea>Введите ваше сообщение
   <p><b>Как по вашему мнению расшифровывается аббревиатура &quot;ОС&quot;?</b></p>
   <p><input type="radio" name="answer" value="a1">Офицерский состав<Br>
   <input type="radio" name="answer" value="a2">Операционная система<Br>
