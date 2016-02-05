4580f32b851b6c64b317424d8c0bc62127a40260
Kalugins <Kalugins@mashinegun>
2016-02-05 11:44:20 +0200

    Добавлен ксс меню

--- application/view/tmp_head.php
+++ application/view/tmp_head.php
@@ -1,8 +1,22 @@
 <html>
     
 <head>
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
 </head>
 <body>
     <div id="head">
-        head
+<ul id="nav">
+<li>
+Пункт 1 меню первого уровня
+<ul>
+<li>Пункт 1 меню второго уровня</li>
+<li>Пункт 2 меню второго уровня</li>
+<li>Пункт 3 меню второго уровня</li>
+</ul>
+</li>
+<li>Пункт 2 меню первого уровня</li>
+<li>Пункт 3 меню первого уровня</li>
+<li>Пункт 4 меню первого уровня</li>
+</ul>
+
     </div>    
