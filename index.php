07430b80461fa25c402ded37e7825bc30c22c865
Kalugins <Kalugins@mashinegun>
2016-02-05 14:30:22 +0200

    добавил колонки

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 include("application/view/tmp_head.php");
 include("application/view/tmp_lft_colon.php");
 include("application/view/tmp_content.php");
+include("application/view/tmp_r_colon.php");
 include("application/view/tmp_bottom.php");
