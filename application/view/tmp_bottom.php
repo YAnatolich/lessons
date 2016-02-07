5828145e61c6ed77d6e716ec7985416f7ea51d6b
YAnatolich <ja.kalugin@gmail.com>
2016-02-07 21:37:36 +0200

    I changed the order used files in index.php. Add form in GuestController.

---
+++ application/view/tmp_bottom.php
@@ -0,0 +1,6 @@
+</div>
+<div id="spacer"> </div>
+</div>
+    <div id="footer">Подвал</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include("application/view/tmp_head.php");
 include("application/view/tmp_lft_colon.php");
-include("application/view/tmp_content.php");
 include("application/view/tmp_r_colon.php");
+include("application/view/tmp_content.php");
 include("application/view/tmp_bottom.php");
