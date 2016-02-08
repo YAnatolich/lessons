68e689c5eb75ba5b63b3820d1255c146ed73b986
Kalugins <Kalugins@mashinegun>
2016-02-08 10:27:26 +0200

    geust contoller was add

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include("application/view/tmp_head.php");
 include("application/view/tmp_lft_colon.php");
-include("application/view/tmp_content.php");
 include("application/view/tmp_r_colon.php");
+include("application/view/tmp_content.php");
+
 include("application/view/tmp_bottom.php");
