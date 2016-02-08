3b365b5e2004e6b210b85de28ebb8877637e1063
Kalugins <Kalugins@mashinegun>
2016-02-08 10:39:15 +0200

    geust contoller was add

--- application/controller/GuestController.php
+++ application/controller/GuestController.php
@@ -1,9 +1,15 @@
 <?php
-include "../../application/view/guest_form.php";
+include("../../application/view/tmp_head.php");
+include("../../application/view/tmp_lft_colon.php");
+include("../../application/view/tmp_r_colon.php");
+include("../../application/view/tmp_content.php");
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+include "../../application/view/guest_form.php";
 echo $_POST["answer"];
+
+include("../../application/view/tmp_bottom.php");
--- index.php
+++ index.php
@@ -3,5 +3,4 @@ include("application/view/tmp_head.php");
 include("application/view/tmp_lft_colon.php");
 include("application/view/tmp_r_colon.php");
 include("application/view/tmp_content.php");
-
 include("application/view/tmp_bottom.php");
