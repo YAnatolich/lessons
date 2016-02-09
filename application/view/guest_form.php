7868de8637fb0e0706733a6ff5010fc54e859084
Kalugins <Kalugins@mashinegun>
2016-02-09 09:56:15 +0200

    Merge origin/master
    Conflicts:
    	public/js/validate.js

--- application/view/guest_form.php
+++ application/view/guest_form.php
@@ -1,12 +1,26 @@
 <form action="GuestController.php" method="POST">
 
-    <input name="name">Введите имя 
-    <input name="email">Введите мыло
+    <input id="username" name="name" >Введите имя 
+    <div id="error"></div>
+    <input id="email" name="email" value="input name">Введите мыло
+    <div id="error1"></div>
     <textarea>
     </textarea>Введите ваше сообщение
   <p><b>Как по вашему мнению расшифровывается аббревиатура &quot;ОС&quot;?</b></p>
-  <p><input type="radio" name="answer" value="a1">Офицерский состав<Br>
+  <p><input type="radio"  name="answer" value="a1">Офицерский состав<Br>
   <input type="radio" name="answer" value="a2">Операционная система<Br>
   <input type="radio" name="answer" value="a3">Большой полосатый мух</p>
   <p><input type="submit"></p>sdfsdf
  </form> 
+<p id="dataset-test" data-name="Поддерживается атрибут dataset">
+  Атрибут dataset не поддерживается
+</p>
+
+<script type="text/javascript">
+  // удобный доступ к данным через dataset
+  // браузерами пока не поддерживается
+  var datasetTest = document.getElementById('dataset-test');
+  if(datasetTest.dataset) {
+    datasetTest.innerHTML = datasetTest.dataset.name;
+  }
+</script>
